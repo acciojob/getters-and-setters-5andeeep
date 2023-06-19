fc1901ccd303e963f3c4f57af5882724e42d9834
Sandeep <newsandeep03@gmail.com>
2023-06-19 13:36:41 +0530

    ans added

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 //complete this code
-class Person {}
+class Person {
+    constructor(name, age){
+        this._name = name;
+        this._age = age;
+    }
+    get name(){
+        return this._name;
+    }
+    get age(){
+        return this._age;
+    }
+    set age(newAge){
+        this._age = newAge;
+    }
+}
 
-class Student extends Person {}
+class Student extends Person {
+    study(){
+        console.log(`${this.name} is studying.`)
+    }
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+    teach(){
+        console.log(`${this.name} is teaching.`)
+    }
+}
+
+// let student = new Student("Alice", 25);
+// student.study();
+
+// let teacher = new Teacher("Mr. Smith", 45);
+// teacher.teach();
+
+
+
+
 
 // Do not change the code below this line
 window.Person = Person;
